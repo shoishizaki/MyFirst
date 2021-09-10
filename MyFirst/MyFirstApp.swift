42b4057e9ca7d9180988b4251f9f2adebaf56740
shoishizaki <shoishizaki1997@gmail.com>
2021-09-10 18:00:56 +0900

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by 石崎將 on 2021/09/10.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
