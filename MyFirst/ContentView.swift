10185daf6f3bd8b237ae33f08b375b9d3233af8a
shoishizaki <shoishizaki1997@gmail.com>
2021-12-27 20:54:09 +0900

    create basic hello app

--- MyFirst/ContentView.swift
+++ MyFirst/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var outputText = "Hello World"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            Text(outputText)
+                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                .foregroundColor(Color.red)
+                .padding()
+
+            Button(action: {
+                if outputText == "Hello World" {
+                    outputText = "Hello, Swift!"
+                } else if outputText == "Hello, Swift!" { outputText = "Hello World" }
+                
+                print("デバックテスト")
+                print(outputText)
+            }) {
+                /*@START_MENU_TOKEN@*/Text("切り替えボタン")
+                    .foregroundColor(Color.white)
+                    .padding(.all)
+                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)/*@END_MENU_TOKEN@*/
+            }
+        }
     }
 }
 
